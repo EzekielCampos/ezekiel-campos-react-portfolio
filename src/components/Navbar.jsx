877adaecea6cd0c1dd46cb1ea02659c8b6d3f976
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-14 22:12:02 -0700

    fixed the header to take the entire width of space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-import { useState } from "react"; 
+import { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { Grid2 as Grid } from "@mui/material";
@@ -13,49 +13,50 @@ export const Nav = () => {
 
   return (
     <Grid
-    container
-    style={{
-      width:'100%',
-      padding: "15px",
-      background: "linear-gradient(90deg, #f5f7fa, #c3cfe2)",
-      color: "#333",
-    }}
-  >
-    <Grid container size={12} alignItems={"center"} marginRight={'15px'}>
-      <Grid size={{ xs: 6, md: 9, sm: 7 }}>
-        <h2
-          style={{
-            fontFamily: "'Playfair Display', sans-serif",
-            fontWeight: 400,
-          }}
-        >
-          Ezekiel Campos
-        </h2>
-      </Grid>
-      <Grid size={{ md: 3, xs: 6, sm: 5 }}>
-        <Tabs value={value} onChange={handleChange}>
-          <Tab
-            value="one"
-            label="About Me"
-            onClick={() => console.log("test")}
-            sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
-          />
-          <Tab
-            value="two"
-            label="My Projects"
-            onClick={() => console.log("test 2")}
-            sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
-          />
-          <Tab
-            value="three"
-            label="Contact Me"
-            onClick={() => console.log("test 3")}
-            sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
-          />
-        </Tabs>
+      container
+      style={{
+        position: "fixed",
+        top: 0,
+        width: "100%",
+        padding: "15px",
+        background: "linear-gradient(90deg, #f5f7fa, #c3cfe2)",
+        color: "#333",
+      }}
+    >
+      <Grid container size={12} alignItems={"center"} marginRight={"15px"}>
+        <Grid size={{ xs: 6, md: 9, sm: 7 }}>
+          <h2
+            style={{
+              fontFamily: "'Playfair Display', sans-serif",
+              fontWeight: 400,
+            }}
+          >
+            Ezekiel Campos
+          </h2>
+        </Grid>
+        <Grid size={{ md: 3, xs: 6, sm: 5 }}>
+          <Tabs value={value} onChange={handleChange}>
+            <Tab
+              value="one"
+              label="About Me"
+              onClick={() => console.log("test")}
+              sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
+            />
+            <Tab
+              value="two"
+              label="My Projects"
+              onClick={() => console.log("test 2")}
+              sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
+            />
+            <Tab
+              value="three"
+              label="Contact Me"
+              onClick={() => console.log("test 3")}
+              sx={{ minWidth: 80, padding: "6px 12px", fontSize: "0.850rem" }} // Adjust the size
+            />
+          </Tabs>
+        </Grid>
       </Grid>
     </Grid>
-  </Grid>
-    
   );
 };
