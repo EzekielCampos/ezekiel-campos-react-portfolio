d45ba510d0660c7afc9253789157d3bc1d767c98
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-15 20:46:49 -0700

    In the card component created a map to display all the projects in individual cards

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,14 +1,26 @@
 
 import image from '../img/profile.jpg';
 
-export const projectData =
+export const projectData =[
 
     {
+        id: 1,
+        name: "Test",
+        description:"Let's GOOO afdafajfkjdasifjaksjf iwj kfjdska jfkadsjfdkalj kljl kj",
+        pic:image,
+        github:"https://github.com/EzekielCampos/ezekiel-campos-react-portfolio",
+        deployed:"https://mail.google.com/mail/u/0/#inbox"
+    },
+
+    {
+        id:2,
         name: "Test",
         description:"Let's GOOO afdafajfkjdasifjaksjf iwj kfjdska jfkadsjfdkalj kljl kj",
         pic:image,
         github:"https://github.com/EzekielCampos/ezekiel-campos-react-portfolio",
         deployed:"https://mail.google.com/mail/u/0/#inbox"
     }
+]
+
 
 
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -7,66 +7,69 @@ import {
   Typography,
   IconButton,
 } from "@mui/material";
-// import Stack from "@mui/material/Stack";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LanguageIcon from "@mui/icons-material/Language"; // For deployed page icon
-import PropTypes from 'prop-types';
-
-// import image from "../../assets/img/profile.jpg";
+import PropTypes from "prop-types";
 
 export const ProjectCard = ({ data }) => {
   console.log(data);
-  return (
-    <Grid size={{ xs: 12, md: 6, s: 4 }}>
-      <Card sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
-        <CardMedia
-          sx={{ height: 600, objectFit: "cover" }}
-          image={data.pic}
-          title="green iguana"
-        />
 
-        {/* Card Content */}
-        <CardContent>
-          <Grid container justifyContent={"center"}>
-            <Typography gutterBottom variant="h5" component="div">
-              {data.name}
-            </Typography>
-          </Grid>
-          <Grid container justifyContent={"center"}>
-            <Typography variant="body2" sx={{ color: "text.secondary" }}>
-              {data.description}
-            </Typography>
-          </Grid>
-        </CardContent>
+  return data.map((data) => {
+    return (
+      <Grid size={{ xs: 12, md: 6, s: 4 }} key={data.id}>
+        <Card sx={{ display: "flex", flexDirection: "column", height: "100%" }}>
+          <CardMedia
+            sx={{ height: 600, objectFit: "cover" }}
+            image={data.pic}
+            title="green iguana"
+          />
 
-        {/* Card Actions */}
-        <CardActions sx={{ justifyContent: "center", marginTop: "auto" }}>
-          {/* GitHub Icon */}
-          <IconButton
-            sx={{ color: "skyBlue" }}
+          {/* Card Content */}
+          <CardContent>
+            <Grid container justifyContent={"center"}>
+              <Typography gutterBottom variant="h5" component="div">
+                {data.name}
+              </Typography>
+            </Grid>
+            <Grid container justifyContent={"center"}>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                {data.description}
+              </Typography>
+            </Grid>
+          </CardContent>
+
+          {/* Card Actions */}
+          <CardActions sx={{ justifyContent: "center", marginTop: "auto" }}>
+            {/* GitHub Icon */}
+            <IconButton
+              sx={{ color: "skyBlue" }}
               onClick={() => window.open(data.github)}
-          >
-            <GitHubIcon style={{ width: 35, height: 35 }} />
-          </IconButton>
+            >
+              <GitHubIcon style={{ width: 35, height: 35 }} />
+            </IconButton>
 
-          {/* Deployed Page Icon */}
-          <IconButton
-            sx={{ color: "#000" }}
-            //   onClick={() => window.open(deployedUrl, "_blank")}
-          >
-            <LanguageIcon style={{ width: 35, height: 35 }} />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </Grid>
-  );
+            {/* Deployed Page Icon */}
+            <IconButton
+              sx={{ color: "#000" }}
+              //   onClick={() => window.open(deployedUrl, "_blank")}
+            >
+              <LanguageIcon style={{ width: 35, height: 35 }} />
+            </IconButton>
+          </CardActions>
+        </Card>
+      </Grid>
+    );
+  });
 };
 
 ProjectCard.propTypes = {
-    data: PropTypes.shape({
-      name: PropTypes.string,      // `title` should be a string
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string, // `title` should be a string
       description: PropTypes.string, // `description` should be a string
-      github:PropTypes.string,
-      pic:PropTypes.string,
-    }).isRequired,
-  };
+      github: PropTypes.string,
+      pic: PropTypes.string,
+    })
+  ).isRequired,
+};
