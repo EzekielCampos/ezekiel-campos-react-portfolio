33325e0dd90e291db8f9f5515f004b1f6bba5699
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-19 01:55:02 -0700

    Skills component will output each skill in the Resume page by mapping through the data

---
+++ src/assets/js/skillsItems.js
@@ -0,0 +1,34 @@
+// import JavascriptIcon from "@mui/icons-material/Javascript";
+
+export const skills = [
+  {
+    id: 1,
+    tech: "JavaScript",
+  },
+  {
+    id: 2,
+
+    tech: "Node.js",
+  },
+  {
+    id: 3,
+
+    tech: "React",
+  },
+  {
+    id: 4,
+
+    tech: "Express.js",
+  },
+
+  {
+    id: 5,
+
+    tech: "MongoDB",
+  },
+  {
+    id: 6,
+
+    tech: "Mongoose",
+  },
+];
--- src/components/UI/Skills.jsx
+++ src/components/UI/Skills.jsx
@@ -3,22 +3,68 @@ import {
     ListItemIcon,
     ListItemText,
   } from "@mui/material";
+  
 
 
-  export const Skills = ()=>{
+  export const Skills = ({data})=>{
 
-    return(
-        <ListItem>
-        <ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
-          •
-        </ListItemIcon>
-        <ListItemText>
-          second Item
-         <ListItemIcon>{mongoIcon}</ListItemIcon>
-        </ListItemText>
-      </ListItem>
+    return(data.map((item)=>{
+        return(
+            <ListItem key={item.id}>
+            <ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+              •
+            </ListItemIcon>
+            <ListItemText>
+              {item.tech}
+            </ListItemText>
+          </ListItem>
+
+        )
+    })
+       
     )
 
-
-
   }
+
+
+
+{/* <ListItem>
+<ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+  •
+</ListItemIcon>
+<ListItemText>
+  First Item
+  <GitHubIcon
+    style={{ width: 20, height: 20, paddingLeft: "5px" }}
+  />
+</ListItemText>
+</ListItem>
+<ListItem>
+<ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+  •
+</ListItemIcon>
+<ListItemText fontSize={10}>
+  second Item
+  {/* <ListItemIcon sx={{ width: 20, height: 20, color:"black" }}>{reactIcon}</ListItemIcon> */}
+  {/* React icon */}
+// </ListItemText>
+// </ListItem>
+// <ListItem>
+// <ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+//   •
+// </ListItemIcon>
+// <ListItemText>
+//   second Item
+//   <JavascriptIcon sx={{ width: 20, height: 20 }} />{" "}
+// </ListItemText>
+// </ListItem>
+
+// <ListItem>
+// <ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+//   •
+// </ListItemIcon>
+// <ListItemText>
+//   second Item
+//   <ListItemIcon>{mongoIcon}</ListItemIcon>
+// </ListItemText>
+// </ListItem> */}
