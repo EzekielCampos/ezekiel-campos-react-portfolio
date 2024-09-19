32fa9949acfce01d61d75a2af4b9beed43d0faf8
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-09-19 00:18:02 -0700

    outlining list items for proficiencies in resume page

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,4 +1,14 @@
-import { Grid2 as Grid, Typography, Link } from "@mui/material";
+import {
+  Grid2 as Grid,
+  Typography,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export const Resume = () => {
   return (
@@ -17,26 +27,42 @@ export const Resume = () => {
         justifyContent={"center"}
         sx={{ background: "grey" }}
       >
-        <Typography variant="h4">Resume</Typography>
+        <Typography variant="h3">Resume</Typography>
       </Grid>
-      <Grid
-        container
-        size={12}
-        justifyContent={"center"}
-        sx={{ background: "skyBlue" }}
-      >
-        <Typography variant="body1">
-          Download my{" "}
-          <Link
-            href="/path/to/your-file.pdf"
-            download="YourFileName.pdf"
-            underline="hover"
-          >
-            Resume
-          </Link>
-        </Typography>
+      <Grid container size={12} sx={{ background: "skyBlue" }}>
+        <Grid size={12} sx={{ background: "white" }}>
+          <Typography textAlign="center" variant="body1">
+            Download my{" "}
+            <Link
+              href="/path/to/your-file.pdf"
+              download="YourFileName.pdf"
+              underline="hover"
+            >
+              Resume
+            </Link>
+          </Typography>
+        </Grid>
+        <Grid size={12}>
+          <Typography variant="h5">Proficiencies:</Typography>
+        </Grid>
+        <Grid size={12}>
+          <List>
+            <ListItem>
+              <ListItemIcon style={{ minWidth: "auto", marginRight: "8px" }}>
+                •
+              </ListItemIcon>
+              <ListItemText>First Item <IconButton sx={{ color: "black" }}>
+                <GitHubIcon style={{ width:20, height: 20, }} />
+              </IconButton></ListItemText>
+              
+            </ListItem>
+            <ListItem>-SOmething</ListItem>
+            <ListItem>-cool</ListItem>
+            <ListItem>-is </ListItem>
+            <ListItem>-about</ListItem>
+          </List>
+        </Grid>
       </Grid>
-     
     </Grid>
   );
 };
