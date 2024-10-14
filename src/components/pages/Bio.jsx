3e39e0a5342440a4459691eade1df1f0a66bd2c2
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-10-13 21:25:42 -0700

    fixing bio positioning

--- src/components/pages/Bio.jsx
+++ src/components/pages/Bio.jsx
@@ -17,7 +17,6 @@ export const Bio = () => {
       }}
     >
       <Grid
-        container
         xs={12}
         md={5}
         display="flex"
@@ -35,12 +34,12 @@ export const Bio = () => {
       </Grid>
 
       <Grid
-      alignContent={"center"}
+        alignItems={"center"}
         size={{ xs: 12, md: 7 }}
         sx={{ paddingRight: "20px", paddingLeft: "20px" }}
       >
         {/* The bio text will be placed next to the image */}
-        <Grid display="flex" justifyContent="center">
+        <Grid display="flex" justifyContent="center" alignContent={"center"}>
           {/* Name header */}
           <Typography variant="h3" component="h1" gutterBottom>
             Ezekiel Campos
@@ -57,21 +56,24 @@ export const Bio = () => {
             Full-Stack Developer
           </Typography>
         </Grid>
-        {/* Short summary of developer */}
-        <Typography variant="body1">
-          Welcome to my portfolio page! My journey in programming began little
-          over a two years ago when I began learning to code in C/C++. I took a
-          certficate course through UCSD and had so much fun learning how code
-          that I was eager for more! I wanted to continue feed my passion for
-          software development. I just recently completed a coding bootcamp
-          through UC Berkeley Extension. The program help improve my skills to
-          be competitive to land a position as a full stack developer. My goal
-          is to find an internship or entry level position. I'm looking to find
-          a company that wants to invest in me so that I can continue to build
-          on my skills as a programmer and a leader. Take a look at some of my
-          work in the projects tab, and if you would like to connect wtih find
-          me on LinkedIn the link is in the footer below.
-        </Typography>
+        <Grid display="flex" justifyContent="center">
+          {/* Short summary of developer */}
+          <Typography variant="body1">
+            Welcome to my portfolio page! My journey in programming began little
+            over a two years ago when I began learning to code in C/C++. I took
+            a certficate course through UCSD and had so much fun learning how
+            code that I was eager for more! I wanted to continue feed my passion
+            for software development. I just recently completed a coding
+            bootcamp through UC Berkeley Extension. The program help improve my
+            skills to be competitive to land a position as a full stack
+            developer. My goal is to find an internship or entry level position.
+            I'm looking to find a company that wants to invest in me so that I
+            can continue to build on my skills as a programmer and a leader.
+            Take a look at some of my work in the projects tab, and if you would
+            like to connect wtih find me on LinkedIn the link is in the footer
+            below.
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
